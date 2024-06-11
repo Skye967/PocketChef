11a2943a8c21147abf7961144d7bf89d9a35711d
skyonide <skye.grossman@gmail.com>
2024-06-10 21:06:23 -0600

    removed page flip temperary

--- app/chef/page.tsx
+++ app/chef/page.tsx
@@ -14,48 +14,6 @@ const Home: React.FC = () => {
   const [recipeList, setRecipeList] = useState<Recipe[] | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-  const [getImages, setGetImages] = useState(false)
-
-  const imageGenerator = async (prompt: string) => {
-    const response = await fetch(`/api/generate-image`, {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ prompt: prompt })
-    })
-    const image = response.json()
-    return image
-  }
-
-
-  const image = async () => {
-    setError('')
-    if (recipeList !== null) {
-      for (let i = 0; i < recipeList.length; i++) {
-        setTimeout(async () => {
-          if (!recipeList[i].imageUrl) {
-            try {
-              const imageUrl = await imageGenerator(recipeList[i].title!)
-              if (imageUrl.data[0].url) {
-                const nextRecipeList = recipeList
-                nextRecipeList[i].imageUrl = imageUrl.data[0].url
-                setRecipeList([...nextRecipeList])
-              }
-            } catch (error) {
-              setGetImages(false)
-              setError('Something went wrong, please try again!');
-              console.error('Something went wrong!', error);
-              throw error
-            }
-          }
-        }, 12000 * (i + 1));
-      }
-    }
-    setGetImages(false)
-    setError('')
-  }
 
   const HandleSubmit = (
     ingredientList: String,
@@ -75,29 +33,18 @@ const Home: React.FC = () => {
       ).then((data: Recipe[] | null) => {
         setRecipeList(data)
         setIsLoading(false)
-        setGetImages(true)
       })
 
     } catch (error) {
       console.error('Something went wrong!', error);
-      setGetImages(false)
       setIsLoading(false)
       setError('Something went wrong, please try again!');
       return
     }
   };
 
-  useEffect(() => {
-    if (getImages && recipeList) {
-      image()
-    }
-  }, [getImages])
-
   return (
-    <main className='chef'>
-      {/* <button className='button-89' onClick={flip}>
-        Go Back
-      </button> */}
+    <main className='chef bg'>
       <RecipeForm onSubmit={HandleSubmit} />
       {!isLoading && error && <ErrorMessage message={error} />}
       {isLoading ? (
@@ -105,12 +52,6 @@ const Home: React.FC = () => {
       ) : (
         <RecipeList recipes={recipeList} />
       )}
-
-
-      <div className="bg">
-
-
-      </div>
     </main >
   );
 }
---
+++ app/util/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+import PageFlip from './components/PageFlip';
+
+
+export default function Home() {
+
+
+    return (
+        <div className='w-full h-full'>
+            <PageFlip/>
+        </div>
+    );
+}
