7917489d4ff39c11bcdcdfc2bc4dd4806f6fcaf7
skyonide <skye.grossman@gmail.com>
2024-09-17 18:14:57 -0500

    got rid of trigger stuff

--- trigger.ts
+++
@@ -1,7 +0,0 @@
-import { TriggerClient } from "@trigger.dev/sdk";
-
-export const client = new TriggerClient({
-  id: "v2-test-project-hZg3",
-  apiKey: process.env.TRIGGER_API_KEY,
-  apiUrl: process.env.TRIGGER_API_URL,
-});
